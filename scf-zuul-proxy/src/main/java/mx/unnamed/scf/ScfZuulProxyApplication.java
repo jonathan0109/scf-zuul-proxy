418d852de65fabee5a41f3b7267f3f2a389a7178
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-15 01:47:23 -0500

    skeleton

---
+++ scf-zuul-proxy/src/main/java/mx/unnamed/scf/ScfZuulProxyApplication.java
@@ -0,0 +1,13 @@
+package mx.unnamed.scf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfZuulProxyApplication.class, args);
+	}
+
+}
